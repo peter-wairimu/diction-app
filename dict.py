a45c0cdee98285bc7563ec7b3ba8df67607f6f51
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-01-05 09:36:50 +0300

    updated python code

---
+++ dict.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# open the file in read mode
+
+text = open('text.txt', 'r')
+
+#create an empty dictionary
+dictionary = dict()
+
+#loop through the lines in the file
+for line in text:
+    #split the line into words
+    words = line.split()
+    #loop through the words
+    for word in words:
+        #if the word is in the dictionary
+        if word in dictionary:
+            #increment the count
+            dictionary[word] += 1
+        #if the word is not in the dictionary
+        else:
+            #add the word to the dictionary
+            dictionary[word] = 1
+    
+#print the dictionary
+for key in dictionary:
+    print(key, dictionary[key])
